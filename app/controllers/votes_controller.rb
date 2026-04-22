7232a3945dbc534bce67aa049049a56dd861ce77
Steve Butler <steve.butler@kyan.com>
2026-04-22 13:32:12 +0100

    refactor(backend): minow tweaks to Voting UI work
    - added ownership check on destroy
    - removed superfluous reload
    - refactor vote lookup to use detect
    - added error handling in vote controller in case of missing votes

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,5 +1,6 @@
 class VotesController < ApplicationController
   before_action :set_nomination, only: [ :create ]
+  before_action :set_vote, only: [ :destroy ]
 
   def create
     vote = Vote.new(user: Current.user, nomination: @nomination)
@@ -18,21 +19,27 @@ class VotesController < ApplicationController
   end
 
   def destroy
-    @vote = Vote.find(params[:id])
+    return head :forbidden unless @vote.user_id == Current.user.id
+
     @nomination = @vote.nomination
     @vote.destroy
-    @nomination.reload
     BroadcastVoteTallyJob.perform_later(@nomination.id)
 
     respond_to do |format|
       format.html { redirect_to @nomination.cycle.club }
       format.turbo_stream
     end
+  rescue ActiveRecord::RecordNotFound
+    redirect_to clubs_path, alert: "Vote not found"
   end
 
   private
 
   def set_nomination
     @nomination = Nomination.includes(:cycle).find(params[:nomination_id])
+  end
+
+  def set_vote
+    @vote = Vote.find(params[:id])
   end
 end
--- app/presenters/nomination_presenter.rb
+++ app/presenters/nomination_presenter.rb
@@ -2,7 +2,7 @@ class NominationPresenter < ApplicationPresenter
   def vote_button
     if current_user.present? && voted_by?(current_user)
       view_context.button_to "Remove Vote",
-        view_context.vote_path(votes.find { |v| v.user_id == current_user.id }),
+        view_context.vote_path(votes.detect { |v| v.user_id == current_user.id }),
         method: :delete
     elsif current_user.present?
       view_context.button_to "Cast Vote",
